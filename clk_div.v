39c26a8ac095da17955c964906a2809be45023ad
Jingnan Huang <huan4194@remote.ecf.utoronto.ca>
2025-11-16 12:50:50 -0500

    UPDATE

--- clk_div.v
+++ clk_div.v
@@ -9,7 +9,7 @@ module clk_div2 (clk_in, reset, clk_out);
     input reset;      // 异步复位，低电平有效
     output clk_out;   // 输出时钟，为输入频率的一半
 
-    reg clk_out;
+    reg clk_out
 
     always @(posedge clk_in or posedge reset)
     begin
