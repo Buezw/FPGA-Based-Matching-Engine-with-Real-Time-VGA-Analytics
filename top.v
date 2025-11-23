3a1c199bb447ae74b7d03f40f0f444482e295441
Andrew An <114970311+Shinoaki798@users.noreply.github.com>
2025-11-23 14:08:53 -0500

    Modify clock signals for order generator and counter

--- top.v
+++ top.v
@@ -25,6 +25,7 @@ module top (CLOCK_50, KEY, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, LEDR, VGA_R, VGA_
     assign reset = ~KEY[0];
 
     wire clk_25;
+    wire slow_clk;
     clk_div2 div25(clk_50, reset, clk_25);
 
     wire [7:0] buy_price;
@@ -41,13 +42,13 @@ module top (CLOCK_50, KEY, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, LEDR, VGA_R, VGA_
     wire halt_signal;
     wire enable_count;
 
-    order_generator generator(clk_50, reset, buy_price, sell_price, KEY);
+    order_generator generator(clk_50, reset, buy_price, sell_price, KEY, slow_clk);
 
     matching_engine engine(clk_50, reset, buy_price, sell_price, match_signal, trade_price, best_bid, best_ask);
 
     controller_fsm controller(clk_50, reset, match_signal, halt_signal, state, enable_count);
 
-    counter trade_counter(clk_50, reset, match_signal, enable_count, trade_count, halt_signal);
+    counter trade_counter(slow_clk, reset, match_signal, enable_count, trade_count, halt_signal);
 
     spread spread_calc(clk_50, reset, match_signal, enable_count, buy_price, sell_price, spread_now);
 
